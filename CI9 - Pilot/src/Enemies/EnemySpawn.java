d9bef1f980d9dde473668ddbbcd3655961078b43
hoanghas4c <hoangha.s4cclub@gmail.com>
2018-08-29 22:43:17 +0700

    framecounter and enemybullet added

--- CI9 - Pilot/src/Enemies/EnemySpawn.java
+++ CI9 - Pilot/src/Enemies/EnemySpawn.java
@@ -8,9 +8,10 @@ import java.util.Random;
 public class EnemySpawn {
     Random random;
     public ArrayList<Enemy> enemies;
-    FrameCounter frameCounter = new FrameCounter(50);
-
-
+    FrameCounter frameCounter = new FrameCounter(30);
+    public EnemySpawn(){
+        random = new Random();
+    }
 
     public void run(){
         frameCounter.run();
